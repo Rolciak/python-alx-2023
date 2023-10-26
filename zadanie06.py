9a5e50be0acf9967f1ae9107d3ce80bc699f3525
Michal <116114893+Rolciak@users.noreply.github.com>
2023-10-26 18:29:51 +0200

    Add files via upload

---
+++ zadanie06.py
@@ -0,0 +1,8 @@
+zdanie = input("Napisz dowolne zdanie: ")
+
+# print(f"{"a" in zdanie = }")
+
+if "a" in zdanie: 
+    print("W zdaniu znajduję się litera A")
+else:
+    print("W zdaniu nie znajduję się litera A")
