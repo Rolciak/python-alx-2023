3112481339b01f76a50c8136d02bc8e0ae03af06
Michal <116114893+Rolciak@users.noreply.github.com>
2023-10-26 17:36:33 +0200

    Add files via upload

---
+++ nauka01.py
@@ -0,0 +1,9 @@
+# print (wyświetlanie funkcji)
+print("dzien dobry")
+print(2 + 2)    # dodawanie
+print(55 - 50)  # odejmowanie
+print(2 * 2)    # mnożenie
+print(16 / 2)   # dzielenie
+print(2 ** 3)   # potęgowanie
+print(100 // 3) # dzielenie z liczbą całkowitą
+print(100 % 3)  # dzielenie z resztą
