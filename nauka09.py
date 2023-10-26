77fb1fe6b630255b1b9902a59b7f55391aaad709
Michal <116114893+Rolciak@users.noreply.github.com>
2023-10-26 18:17:02 +0200

    Add files via upload

---
+++ nauka09.py
@@ -0,0 +1,22 @@
+a = 100
+b = 60
+
+waluta = "EUR"
+
+print(f"{a + b = }")
+print(f"{a > b = }")
+
+
+print(f"{waluta == "USD" = }")
+print(type(123))
+print(type(123.5))
+print(type("abc"))
+print(type(True))
+print(None)
+print(type(None))
+
+zdanie = "ala ma kota"
+
+# print("kot" in zdanie)
+print(f"{"kot" in zdanie = }")
+print(f"{"pies" in zdanie = }")
