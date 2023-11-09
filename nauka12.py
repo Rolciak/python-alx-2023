ae1e22d6e0b3da9024af1ee9647295832818cc3c
Michal <116114893+Rolciak@users.noreply.github.com>
2023-11-09 18:47:06 +0100

    Add files via upload

---
+++ nauka12.py
@@ -0,0 +1,8 @@
+cena = 25_500_000   
+liczba = 0b1000101010101111
+
+print(liczba)
+print(bin(liczba))
+print(f"Binarnie: {liczba:b}")
+print(f"Binarnie: {liczba:20b}")
+print(f"Binarnie: {liczba:<20b}")
