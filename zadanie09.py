62283bee181eca584e5ae8cce0fa2a99fac7bf02
Michal <116114893+Rolciak@users.noreply.github.com>
2023-11-16 19:25:58 +0100

    Add files via upload

--- zadanie09.py
+++ zadanie09.py
@@ -1,5 +1,14 @@
+#cenzurowany_email = (email[0] + '_____________') + email[15:17] + '________'
+
 email = input("Podaj adres e-mail: ")
 
-cenzurowany_email = (email[0] + '_____________') + email[15:17] + '________'
+pierwsza_litera = email[0]
+nazwa, domena = email.split("@")
+#pierwsza_litera_domeny = pozycja_at + 1
 
-print(f"Twój ocenzurowany email: {cenzurowany_email}")
+ocenzurowana_nazwa = nazwa[0] + '_' * (len(nazwa) - 2) + nazwa[-1]
+ocenzurowana_domena = domena[0] + '_' * (len(domena) - 2) + domena[-1]
+
+ocenzurowany_email = ocenzurowana_nazwa + '@' + ocenzurowana_domena
+
+print(f"Twój ocenzurowany email: {ocenzurowany_email}")
