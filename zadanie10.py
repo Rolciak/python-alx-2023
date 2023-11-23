456a120a184bbf73030d5693f07fe35bb732e7f2
Michal <116114893+Rolciak@users.noreply.github.com>
2023-11-23 18:26:01 +0100

    Add files via upload

---
+++ zadanie10.py
@@ -0,0 +1,19 @@
+miesiac = int(input("Podaj numer miesiaca: "))
+
+numery_miesiaca = [
+    ' ',
+    'Styczen', 
+    'Luty', 
+    'Marzec', 
+    'Kwiecien', 
+    'Maj', 
+    'Czerwiec', 
+    'Lipiec', 
+    'Sierpien', 
+    'Wrzesien', 
+    'Pazdziernik', 
+    'Listopad', 
+    'Grudzien'
+]
+
+print(f"Miesiąc: {numery_miesiaca[miesiac]}")
