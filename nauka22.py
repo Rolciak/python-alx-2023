d6703d663a30f06e8016a962f1c4c2d24d7ce809
Michal <116114893+Rolciak@users.noreply.github.com>
2023-12-07 19:19:37 +0100

    Add files via upload

--- nauka22.py
+++ nauka22.py
@@ -1,18 +1,47 @@
 imiona = [
-    'mateusz',
-    'adam',
-    'michal',
-    'iwo',
-    'stanislaw',
-    'artem',
+    'Adam',
+    'Ola',
+    'Mateusz',
+    'Michał',
+    'Iwo',
+    'Stanisław',
+    'Artem',
+    'Władysław',
+    'Karina',
+    'Ala',
+    'Maria',
 ]
-
+max_dlugosc = 0
+min_dlugosc = None
 for imie in imiona:
     # TODO: przy każdej osobie napisać długość imienia
     print('----------')
     print(f'* Imię: {imie}')
+    dlugosc_imienia = len(imie)
+    if dlugosc_imienia > max_dlugosc:
+        max_dlugosc = dlugosc_imienia
+    if min_dlugosc is None or dlugosc_imienia < min_dlugosc:
+        min_dlugosc = dlugosc_imienia
     print(f'* Długość znaków: {len(imie)}')
 print('----------')
 dodanie_imion = "".join(imiona)
 suma_imiona = len(dodanie_imion)
 print(f'* Długość sumeryczna: {suma_imiona}')
+print(f"* Długość najdluzszego imienia: {max_dlugosc}")
+print(f"* Długość najkrotszego imienia: {min_dlugosc}")
+
+for imie in imiona:
+    if len(imie) == min_dlugosc:
+        print(f"{imie} Najkrótrsze")
+    if len(imie) == max_dlugosc:
+        print(f"{imie} Najdłuższe")
+
+print("Męskie: (bez a na końcu)")
+for imie in imiona:
+    if not imie.endswith('a'):
+        print(f"- {imie}")
+
+print("Damskie: (z a na końcu)")
+for imie in imiona:
+    if imie.endswith('a'):
+        print(f"- {imie}")
